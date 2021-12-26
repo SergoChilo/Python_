af24fb2376348318bb7ec4b3bb8dae5af315c9f1
Sergey <chilingaryan1997@internet.ru>
2021-12-26 22:56:43 +0400

    Hangman

--- Hangman.py
+++ Hangman.py
@@ -5,8 +5,6 @@ from words import word_list
 def get_word():
     word = random.choice(word_list)
     return word.upper()
-
-
 def play(word):
     word_completion = "_" * len(word)
     guessed = False
